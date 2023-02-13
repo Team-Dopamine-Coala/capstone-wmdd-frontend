79a378f6f462598130578b58d719208303af0f6a
Danika Enad <danika.enad@gmail.com>
2023-02-12 19:27:30 -0800

    preliminary login page created

---
+++ src/components/screens/LoginScreen.js
@@ -0,0 +1,13 @@
+import { Box, Container, Text, View, VStack } from 'native-base';
+import React from 'react';
+import LoginForm from '../forms/LoginForm';
+
+const Login = () => {
+    return (
+        <View>
+            <LoginForm />
+        </View>
+    )
+}
+
+export default Login;
