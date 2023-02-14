d850423a618abbb729b87c770ebef9f4f634f56a
Danika Enad <danika.enad@gmail.com>
2023-02-13 20:49:32 -0800

    preliminary create account setup

---
+++ src/components/forms/CreateAccountForm.js
@@ -0,0 +1,33 @@
+import { FormControl, Input, Text, VStack, Icon, Pressable, Button, Box, Center } from 'native-base';
+import { MaterialIcons } from "@expo/vector-icons";
+import React, { useState } from 'react';
+
+const CreateAccountForm = props => {
+
+    return(
+        <VStack justifyContent="center" space={2} py={30} px={15}>
+            <FormControl>
+                <Box py={2}>
+                <FormControl.Label>First Name</FormControl.Label>
+                <Input 
+                    placeholder="First Name"
+                    variant="underlined"
+                />
+                </Box>
+                <Box py={2}>
+                <FormControl.Label>Last Name</FormControl.Label>
+                <Input 
+                    placeholder="Last Name"
+                    variant="underlined"
+                />
+                </Box>
+                <Button 
+                    mt={5}
+                    onPress={() => {props.navigation.navigate("SignUpFormTwo")}}
+                >Next</Button>
+            </FormControl>
+        </VStack>
+    )
+}
+
+export default CreateAccountForm;
--- src/components/stacks/AppStack.js
+++ src/components/stacks/AppStack.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import { View } from 'native-base'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import SignUpScreenTwo from '../screens/SignUpScreenTwo';
+
+
+const Stack = createNativeStackNavigator();
 
 const AppStack = () => {
   return (
-    <View>Hey</View>
+    <Stack.Navigator>
+      <Stack.Screen name="SignUpFormTwo" component={SignUpScreenTwo} />
+    </Stack.Navigator>
+    // <View>Hey</View>
   )
 }
 
