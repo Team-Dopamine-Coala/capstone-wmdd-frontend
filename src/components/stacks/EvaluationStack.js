989bd006955dc728b07b555935d9a44d05eb480d
Elmer Balbin Jr <contact@elmer.dev>
2023-02-16 00:49:42 -0800

    evaluation and stack updates

--- src/components/stacks/EvaluationStack.js
+++ src/components/stacks/EvaluationStack.js
@@ -1,14 +1,17 @@
-import IndexScreen from "../screens/Evaluation/IndexScreen"
-
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
-
-const Stack = createNativeStackNavigator()
+import React from 'react'
+import { View, Text, Box } from 'native-base'
 
 const EvaluationStack = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Evaluation Index" component={IndexScreen} />
-    </Stack.Navigator>
+    <View p={5}>
+      <Box width="100%" height="100px" position="relative">
+        <Box bg="#ffc0cb" width="100%" height="90%" borderRadius="md" shadow={9} position="absolute" top="5%">
+        </Box>
+        <Box ml={4} p={3} bg="#ffffff" flex={1} height="100%" borderRadius="md" shadow={5}>
+          <Text>Hello World</Text>
+        </Box>
+      </Box>
+    </View>
   )
 }
 
