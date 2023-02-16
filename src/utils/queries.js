d5b9dfb1e43bcf4331b7c00d557fbb57eec313f8
Elmer Balbin Jr <contact@elmer.dev>
2023-02-16 00:49:42 -0800

    evaluation and stack updates

---
+++ src/utils/queries.js
@@ -0,0 +1,21 @@
+import { BACKEND_BASE_URL } from "./static"
+
+
+// EVALUATIONS
+export const getEvaluationsByClass = async (classid) => {
+  if (classid) {
+    const res = await fetch(`${BACKEND_BASE_URL}/api/evaluation/${classid}`)
+    const data = await res.json()
+
+    return data
+  }
+}
+
+export const getEvaluationsByStudentId = async (studentid) => {
+  if (studentid) {
+    const res = await fetch(`${BACKEND_BASE_URL}/api/evaluation/student/${studentid}`)
+    const data = await res.json()
+
+    return data
+  }
+}
