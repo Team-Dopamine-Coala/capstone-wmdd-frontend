612042f0219e7337ac142c9b0e50f099f06195ec
Elmer Balbin Jr <contact@elmer.dev>
2023-02-16 19:25:43 -0800

    evaluation updates and new packages

--- src/components/screens/Evaluation/IndexScreen.js
+++ src/components/screens/Evaluation/IndexScreen.js
@@ -2,6 +2,10 @@ import { Center, Heading, ScrollView, Text, View, Box } from 'native-base'
 import CalendarStrip from 'react-native-calendar-strip';
 
 const IndexScreen = () => {
+  const dateSelected = (date) => {
+    console.log(date)
+  }
+
   return (
     <ScrollView p={3}>
       <Center>
@@ -9,8 +13,48 @@ const IndexScreen = () => {
       </Center>
       
       <CalendarStrip
-        style={{height:150, paddingTop: 20, paddingBottom: 10}}
-      />
+      scrollable
+      style={{height: 130, paddingTop: 20, paddingBottom: 10}}
+      calendarHeaderStyle={{
+        fontSize: 18,
+        alignSelf: 'flex-start',
+      }}
+      selectedDate={new Date()}
+      onDateSelected={dateSelected}
+      minDate="2023-01-01"
+      maxDate="2023-12-31"
+      dayContainerStyle={{
+        paddingBottom: 4
+      }}
+      dateNameStyle={{
+        marginBottom: 6,
+        fontSize: 13,
+        textTransform: 'capitalize'
+      }}
+      dateNumberStyle={{
+        paddingBottom: 4,
+        fontSize: 16,
+        fontWeight: '300'
+      }}
+      highlightDateNameStyle={{
+        marginBottom: 6,
+        fontSize: 13,
+        textTransform: 'capitalize'
+      }}
+      highlightDateNumberStyle={{
+        color: '#ffffff',
+        fontSize: 16
+      }}
+      highlightDateNumberContainerStyle={{
+        backgroundColor: '#40506A',
+        borderRadius: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignContent: 'center',
+        minWidth: 25,
+        minHeight: 25
+      }}
+    />
       
       <Box>
         <Box></Box>
