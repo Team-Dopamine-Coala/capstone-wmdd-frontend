446fddb4acad6d2f625f659cc45442b5089aabd9
Elmer Balbin Jr <contact@elmer.dev>
2023-02-17 01:35:57 -0800

    evaluation update

--- src/components/stacks/EvaluationStack.js
+++ src/components/stacks/EvaluationStack.js
@@ -1,13 +1,27 @@
 import IndexScreen from "../screens/Evaluation/IndexScreen"
+import EvaluationStudentListScreen from "../screens/Evaluation/EvaluationStudentListScreen"
 
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { createStackNavigator, TransitionPresets } from "@react-navigation/stack"
 
-const Stack = createNativeStackNavigator()
+const Stack = createStackNavigator()
+
+const TransitionScreenOptions = {
+  ...TransitionPresets.SlideFromRightIOS,
+};
 
 const EvaluationStack = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Evaluation Index" component={IndexScreen} />
+    <Stack.Navigator screenOptions={TransitionScreenOptions}>
+      <Stack.Screen name="Evaluation Index" component={IndexScreen} options={{ headerShown: false }} />
+      <Stack.Screen
+        name="Evaluation Student List"
+        component={EvaluationStudentListScreen}
+        options={({ route }) => ({
+          title: 'Gymnastics Evaluation',
+          headerBackTitle: 'Evaluation',
+          headerTitleAlign: 'center'
+        })}
+      />
     </Stack.Navigator>
   )
 }
