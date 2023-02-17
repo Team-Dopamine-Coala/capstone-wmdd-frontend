037a5680c2931541386d645c77e2ef92ac163546
Elmer Balbin Jr <contact@elmer.dev>
2023-02-16 22:22:29 -0800

    file reorganization

--- src/components/screens/Attendance/IndexScreen.js
+++ src/components/screens/Attendance/IndexScreen.js
@@ -1,50 +1,10 @@
-import { Text, VStack, Center } from 'native-base'
-import { useState, useEffect } from 'react'
-import ClassList from "./ClassList/ClassList"
-import Calendar from "./Calendar/Calendar"
-import WelcomeCard from "./Card/WelcomeCard"
+import { Text, View } from 'native-base'
 
-import { getClassesOfCoach, getAllAttendance } from '../../../utils/queries';
-
-
-const IndexScreen = ({ navigation }) => {
-  const {userToken} = useContext(AuthContext)
-  const [classes, setClasses] = useState([]);
-
-  useEffect(() => {
-    setIsLoading(true)
-    getClassesOfCoach('63fcf0bd354e8150f45dd4d2', userToken).then(
-      data => {
-        setClasses(data)
-        setIsLoading(false)
-      },
-      error => {
-        throw error
-      }
-    )
-    
-  }, [dateSelected])
-
-    getClasses() 
-
-  }, [])
-
-  // Fetch Classes
-const fetchClasses = async () => {
-  // const res = await fetch(`${process.env.REACT_APP_API_URL}/api/class/63e065e53e54d66c36ab24ec/63e9fcf20386d6f0fd9053b3`);
-  const res = await fetch("http://3.84.131.140:3000/api/class/63e9fcf20386d6f0fd9053b3");
-  const data = await res.json();
-  return data;
-};
-
+const IndexScreen = () => {
   return (
-    <VStack p={3} pb={20} bgColor="#ffffff" flex={1}>
-
-      {/* <Text>This is index screen of Attendance</Text> */}
-      <WelcomeCard/>
-      <Calendar />
-      <ClassList classes={classes} />
-    </VStack>
+    <View>
+      <Text>This is index screen of Attendance</Text>
+    </View>
   )
 }
 
--- src/components/stacks/AttendanceStack.js
+++ src/components/stacks/AttendanceStack.js
@@ -1,39 +1,14 @@
 import IndexScreen from "../screens/Attendance/IndexScreen"
-import AttendanceStudentList from "../screens/Attendance/Screens/AttendanceStudentList"
-import CompletedAttendance from "../screens/Attendance/Screens/CompletedAttendance"
 
-import { createStackNavigator, TransitionPresets } from "@react-navigation/stack"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
-const Stack = createStackNavigator()
-
-const TransitionScreenOptions = {
-  ...TransitionPresets.SlideFromRightIOS,
-};
+const Stack = createNativeStackNavigator()
 
 const AttendanceStack = () => {
   return (
-    <Stack.Navigator screenOptions={TransitionScreenOptions}>
-      <Stack.Screen name="Attendance Index" component={IndexScreen} options={{ headerShown: false }} />
-      <Stack.Screen 
-        name="Attendance Student List"
-        component={AttendanceStudentList}
-        options={({ route }) => ({
-          title: 'Gymnastics',
-          // headerBackTitle: 'Attendance',
-          headerTitleAlign: 'center'
-        })}
-      />
-       <Stack.Screen 
-        name="Completed Attendance"
-        component={CompletedAttendance}
-        options={({ route }) => ({
-          title: 'Gymnastics',
-          // headerBackTitle: 'Attendance',
-          headerTitleAlign: 'center'
-        })}
-      />
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Attendance Index" component={IndexScreen} />
     </Stack.Navigator>
-    
   )
 }
 
