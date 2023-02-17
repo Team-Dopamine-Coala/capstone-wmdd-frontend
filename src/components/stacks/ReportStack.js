b4483cb7999c0bb89905a68679c012109fdef2dd
Elmer Balbin Jr <contact@elmer.dev>
2023-02-17 01:42:08 -0800

    added report

--- src/components/stacks/ReportStack.js
+++ src/components/stacks/ReportStack.js
@@ -1,24 +1,13 @@
 import IndexScreen from "../screens/Report/IndexScreen"
 
-import { createStackNavigator, TransitionPresets } from "@react-navigation/stack"
+import { createStackNavigator } from "@react-navigation/stack"
 
 const Stack = createStackNavigator()
 
-const TransitionScreenOptions = {
-  ...TransitionPresets.SlideFromRightIOS,
-};
-
 const ReportStack = () => {
   return (
-    <Stack.Navigator screenOptions={TransitionScreenOptions}>
-      <Stack.Screen name="Report Index" component={IndexScreen}
-        options={{
-          title: 'Reports',
-          headerTitleAlign: 'center',
-          headerTransparent: true,
-          headerTintColor: '#ffffff'
-        }}
-      />
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Report Index" component={IndexScreen} />
     </Stack.Navigator>
   )
 }
