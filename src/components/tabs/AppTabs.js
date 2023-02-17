dc4fe9c95e6fe48ff169938e134ce676a41b988f
Elmer Balbin Jr <contact@elmer.dev>
2023-02-16 19:25:43 -0800

    evaluation updates and new packages

--- src/components/tabs/AppTabs.js
+++ src/components/tabs/AppTabs.js
@@ -7,6 +7,7 @@ import StudentsScreen from '../screens/students/StudentsScreen';
 import { Ionicons } from '@expo/vector-icons'
 import { Icon, View, Box, Text } from 'native-base'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import { BlurView } from 'expo-blur';
 
 const Tab = createBottomTabNavigator();
@@ -14,7 +15,15 @@ const Tab = createBottomTabNavigator();
 const AppTabs = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Navigator
+            screenOptions={({ route }) => ({
+                headerShown: false,
+                tabBarActiveTintColor: '#655AE9',
+                tabBarInactiveTintColor: '#667080',
+                tabBarStyle:{ position: 'absolute', height: 60 },
+                tabBarLabelStyle:{ paddingBottom: 6, fontSize: 12 }
+            })}
+      >
         <Tab.Screen name="Attendance" component={AttendanceScreen} options={{
           tabBarIcon: ({ color }) => (
             <Icon size={5} color={color} as={<Ionicons name='ios-calendar' />} />
@@ -40,10 +49,26 @@ const AppTabs = () => {
             <Icon size={5} color={color} as={<Ionicons name='ios-people' />} />
           )
         }} />
-        <Tab.Screen name="Evaluation" component={EvaluationStack} />
-        <Tab.Screen name="Reports" component={ReportsScreen} />
-        <Tab.Screen name="Curriculum" component={CurriculumScreen} />
-        <Tab.Screen name="Students" component={StudentsScreen} />
+        <Tab.Screen name="Evaluation" component={EvaluationStack} options={{
+          tabBarIcon: ({ color }) => (
+            <Icon size={5} ml={2} color={color} as={<Ionicons name='ios-search' />} />
+          )
+        }} />
+        <Tab.Screen name="Reports" component={ReportsScreen} options={{
+          tabBarIcon: ({ color }) => (
+            <Icon size={5} ml={2} color={color} as={<Ionicons name='ios-search' />} />
+          )
+        }} />
+        <Tab.Screen name="Curriculum" component={CurriculumScreen} options={{
+          tabBarIcon: ({ color }) => (
+            <Icon size={5} ml={2} color={color} as={<Ionicons name='ios-search' />} />
+          )
+        }} />
+        <Tab.Screen name="Students" component={StudentsScreen} options={{
+          tabBarIcon: ({ color }) => (
+            <Icon size={5} ml={2} color={color} as={<Ionicons name='ios-search' />} />
+          )
+        }} />
       </Tab.Navigator>
   )
 }
