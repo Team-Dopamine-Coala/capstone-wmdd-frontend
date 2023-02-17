8285d4928aac00d5c0253e55e33d0b528bce33ba
Elmer Balbin Jr <contact@elmer.dev>
2023-02-17 01:35:57 -0800

    evaluation update

---
+++ src/components/screens/Evaluation/listItem/ClassListItem.js
@@ -0,0 +1,25 @@
+import { Box, HStack, VStack, Text, Button, Heading } from 'native-base'
+
+const ClassListItem = ({ item, navigation }) => {
+  return (
+    <Box mb={3} p={5} bgColor="#EEF1F4" borderRadius="lg">
+      <VStack>
+        <HStack>
+          <VStack space={1} mb={2}>
+            <Heading fontSize={22}>{item.title}</Heading>
+            <Text>{item.startTime} - {item.endTime}</Text>
+          </VStack>
+        </HStack>
+
+        <Button
+          bgColor="#667080"
+          onPress={() => {
+            navigation.navigate('Evaluation Student List')
+          }}
+        ><Text fontWeight="700" color="#ffffff">Start Evaluation</Text></Button>
+      </VStack>
+    </Box>
+  )
+}
+
+export default ClassListItem
--- src/components/screens/Evaluation/lists/ClassList.js
+++ src/components/screens/Evaluation/lists/ClassList.js
@@ -1,13 +1,12 @@
 import { Box, FlatList, Text } from "native-base"
+import ClassListItem from "../listItem/ClassListItem"
 
 const ClassList = ({ classes, navigation }) => {
   return (
     <FlatList
       data={classes}
       renderItem={({ item }) => (
-        <Box bgColor="#EEF1F4" borderRadius="md">
-          <Text>{item.title}</Text>
-        </Box>
+        <ClassListItem item={item} navigation={navigation} />
       )}
       keyExtractor={item => item._id}
       showsHorizontalScrollIndicator={false}
