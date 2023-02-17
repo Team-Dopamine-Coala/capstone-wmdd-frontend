3f6194b6bfd15c16a17fc637871d35657492bd4c
Elmer Balbin Jr <contact@elmer.dev>
2023-02-17 10:52:42 -0800

    evaluation update

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -1,9 +1,20 @@
-import { BACKEND_BASE_URL, ANDROID_EMU_BASE_URL } from "./static"
+import { BACKEND_BASE_URL, ANDROID_EMU_BASE_URL, AWS_BACKEND_BASE_URL } from "./static"
 
 // CLASSES
 export const getClassesOfCoach = async (coachid) => {
   if (coachid) {
-    const res = await fetch(`${ANDROID_EMU_BASE_URL}/api/class/${coachid}`)
+    const res = await fetch(`${AWS_BACKEND_BASE_URL}/api/class/${coachid}`)
+    const data = await res.json()
+
+    return data
+  }
+}
+
+
+// STUDENTS
+export const getStudentsByClass = async (classid) => {
+  if (classid) {
+    const res = await fetch(`${AWS_BACKEND_BASE_URL}/api/student/${classid}`)
     const data = await res.json()
 
     return data
@@ -13,7 +24,7 @@ export const getClassesOfCoach = async (coachid) => {
 // EVALUATIONS
 export const getEvaluationsByClass = async (classid) => {
   if (classid) {
-    const res = await fetch(`${ANDROID_EMU_BASE_URL}/api/evaluation/${classid}`)
+    const res = await fetch(`${AWS_BACKEND_BASE_URL}/api/evaluation/${classid}`)
     const data = await res.json()
 
     return data
@@ -22,7 +33,7 @@ export const getEvaluationsByClass = async (classid) => {
 
 export const getEvaluationsByStudentId = async (studentid) => {
   if (studentid) {
-    const res = await fetch(`${ANDROID_EMU_BASE_URL}/api/evaluation/student/${studentid}`)
+    const res = await fetch(`${AWS_BACKEND_BASE_URL}/api/evaluation/student/${studentid}`)
     const data = await res.json()
     return data
   }
