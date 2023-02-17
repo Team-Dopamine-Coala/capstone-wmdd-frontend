c55df271a5d64a8137867976f03ff4dd254d7d54
Elmer <contact@elmer.dev>
2023-02-16 16:37:30 -0800

    evaluation updates

--- src/components/stacks/EvaluationStack.js
+++ src/components/stacks/EvaluationStack.js
@@ -1,17 +1,14 @@
-import React from 'react'
-import { View, Text, Box } from 'native-base'
+import IndexScreen from "../screens/Evaluation/IndexScreen"
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+
+const Stack = createNativeStackNavigator()
 
 const EvaluationStack = () => {
   return (
-    <View p={5}>
-      <Box width="100%" height="100px" position="relative">
-        <Box bg="#ffc0cb" width="100%" height="90%" borderRadius="md" shadow={9} position="absolute" top="5%">
-        </Box>
-        <Box ml={4} p={3} bg="#ffffff" flex={1} height="100%" borderRadius="md" shadow={5}>
-          <Text>Hello World</Text>
-        </Box>
-      </Box>
-    </View>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Evaluation Index" component={IndexScreen} />
+    </Stack.Navigator>
   )
 }
 
