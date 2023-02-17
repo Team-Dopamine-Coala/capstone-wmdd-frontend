555c1f1d9773dbd13dea7c3049d38b9e12425c69
Elmer Balbin Jr <contact@elmer.dev>
2023-02-17 10:52:42 -0800

    evaluation update

---
+++ src/components/screens/Evaluation/EvaluationIndividualStudent.js
@@ -0,0 +1,11 @@
+import { Heading, Text, View } from "native-base"
+
+const EvaluationIndividualStudent = () => {
+  return (
+    <View>
+      <Heading>This is the screen for individual evaluation</Heading>
+    </View>
+  )
+}
+
+export default EvaluationIndividualStudent
--- src/utils/static.js
+++ src/utils/static.js
@@ -1,2 +1,3 @@
 export const BACKEND_BASE_URL = 'http://localhost:5002'
 export const ANDROID_EMU_BASE_URL = 'http://10.0.2.2'
+export const AWS_BACKEND_BASE_URL = 'http://3.84.131.140:3000'
