c3ff297ce3b15a25182122cfeff21edbc5f4af59
Iris Chen <jaminitaa0322@gmail.com>
2023-03-03 07:38:21 -0800

    update attendance stack, add attendance screen-student list & student card

--- src/components/screens/Attendance/IndexScreen.js
+++ src/components/screens/Attendance/IndexScreen.js
@@ -4,19 +4,17 @@ import ClassList from "./ClassList/ClassList"
 import Calendar from "./Calendar/Calendar"
 import WelcomeCard from "./Card/WelcomeCard"
 import Loading from '../../layout/Loading'
-import { AuthContext } from '../../context/AuthContext';
 
 import { getClassesOfCoach } from '../../../utils/queries';
 
 const IndexScreen = ({ navigation }) => {
-  const {userToken} = useContext(AuthContext)
   const [classes, setClasses] = useState([]);
   const [dateSelected, setSelectedDate] = useState(new Date())
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     setIsLoading(true)
-    getClassesOfCoach('63fcf0bd354e8150f45dd4d2', userToken).then(
+    getClassesOfCoach('63fcf0bd354e8150f45dd4d2').then(
       data => {
         setClasses(data)
         setIsLoading(false)
@@ -37,7 +35,7 @@ const IndexScreen = ({ navigation }) => {
       {/* <Text>This is index screen of Attendance</Text> */}
       <WelcomeCard/>
       <Calendar />
-      {isLoading ? <Loading /> : <ClassList classes={classes} navigation={navigation} dateSelected={dateSelected}/>}
+      {isLoading ? <Loading /> : <ClassList classes={classes} navigation={navigation} />}
     </VStack>
   )
 }
