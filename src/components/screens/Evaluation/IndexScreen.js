bbd85a8654036af156fb7c5d576727688d63380d
Elmer Balbin Jr <contact@elmer.dev>
2023-03-02 23:10:19 -0800

    evaluation screen update

--- src/components/screens/Evaluation/IndexScreen.js
+++ src/components/screens/Evaluation/IndexScreen.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { Center, Heading, VStack } from 'native-base'
+import { Box, Center, Heading, VStack } from 'native-base'
 import CalendarStrip from 'react-native-calendar-strip';
 import moment from 'moment';
 
@@ -17,7 +17,7 @@ const IndexScreen = ({ navigation }) => {
     console.log(dateSelected)
 
     setIsLoading(true)
-    getClassesOfCoach('63e9fcf20386d6f0fd9053b3').then(
+    getClassesOfCoach('63fcf0bd354e8150f45dd4d2').then(
       data => {
         setClasses(data)
         setIsLoading(false)
@@ -34,57 +34,49 @@ const IndexScreen = ({ navigation }) => {
   }
 
   return (
-    <VStack p={3} pb={20} bgColor="#ffffff" flex={1}>
-      <Center>
-        <Heading>Evaluation</Heading>
-      </Center>
+    <VStack pt="50px" pb={20} flex={1} bgColor="#F4903F">
+      <Box bgColor="#ffffff" borderTopLeftRadius={20} borderTopRightRadius={20}>
+        <CalendarStrip
+        scrollable
+        style={{height: 130, paddingTop: 20, paddingBottom: 10}}
+        calendarHeaderStyle={{
+          fontSize: 18,
+          alignSelf: 'flex-start',
+        }}
+        startingDate={moment().subtract(3, 'days')}
+        selectedDate={moment()}
+        onDateSelected={onDateClick}
+        minDate="2023-01-01"
+        maxDate="2023-12-31"
+        dayContainerStyle={{
+          paddingBottom: 4
+        }}
+        dateNameStyle={{
+          marginBottom: 6,
+          fontSize: 13,
+          textTransform: 'capitalize'
+        }}
+        dateNumberStyle={{
+          paddingBottom: 4,
+          fontSize: 16,
+          fontWeight: '300'
+        }}
+        highlightDateNameStyle={{
+          color: '#F4903F',
+          marginBottom: 6,
+          fontSize: 13,
+          textTransform: 'capitalize'
+        }}
+        highlightDateNumberStyle={{
+          color: '#F4903F',
+          fontSize: 16
+        }}
+        highlightDateNumberContainerStyle={{
+        }}
+      />
       
-      <CalendarStrip
-      scrollable
-      style={{height: 130, paddingTop: 20, paddingBottom: 10}}
-      calendarHeaderStyle={{
-        fontSize: 18,
-        alignSelf: 'flex-start',
-      }}
-      startingDate={moment().subtract(3, 'days')}
-      selectedDate={moment()}
-      onDateSelected={onDateClick}
-      minDate="2023-01-01"
-      maxDate="2023-12-31"
-      dayContainerStyle={{
-        paddingBottom: 4
-      }}
-      dateNameStyle={{
-        marginBottom: 6,
-        fontSize: 13,
-        textTransform: 'capitalize'
-      }}
-      dateNumberStyle={{
-        paddingBottom: 4,
-        fontSize: 16,
-        fontWeight: '300'
-      }}
-      highlightDateNameStyle={{
-        marginBottom: 6,
-        fontSize: 13,
-        textTransform: 'capitalize'
-      }}
-      highlightDateNumberStyle={{
-        color: '#ffffff',
-        fontSize: 16
-      }}
-      highlightDateNumberContainerStyle={{
-        backgroundColor: '#40506A',
-        borderRadius: 40,
-        justifyContent: 'center',
-        alignItems: 'center',
-        alignContent: 'center',
-        minWidth: 25,
-        minHeight: 25
-      }}
-    />
-    
-    {isLoading ? <Loading /> : <ClassList classes={classes} navigation={navigation} />}
+      {isLoading ? <Loading /> : <ClassList classes={classes} navigation={navigation} />}
+    </Box>
 
     </VStack>
   )
