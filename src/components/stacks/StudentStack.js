8a58451164785822431d28a28166ae7d9b025fea
KEIKO SHIMIZU <106935804+KeikoShimizu@users.noreply.github.com>
2023-03-04 20:03:32 -0800

    student stack title route added

--- src/components/stacks/StudentStack.js
+++ src/components/stacks/StudentStack.js
@@ -11,17 +11,32 @@ const Stack = createStackNavigator()
 const StudentStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Student Index" component={IndexScreen} />
+      <Stack.Screen 
+        name="Student Index" 
+        component={IndexScreen}
+        options={({ route }) => ({
+          title: 'Students',
+          headerTitleAlign: 'center'
+        })}
+      />
       <Stack.Screen 
         name="Student Detail" 
         component={StudentDetail}
         options={({ route }) => ({
           title: 'Student Profile',
-          headerBackTitle: 'Student List',
+          // headerBackTitle: 'Student List',
           headerTitleAlign: 'center'
         })}
       />
-      <Stack.Screen name="Student Profile" component={ViewProfileScreen}/>
+      <Stack.Screen 
+        name="Student Profile" 
+        component={ViewProfileScreen}
+        options={({ route }) => ({
+          title: 'Student Profile',
+          // headerBackTitle: 'Student List',
+          headerTitleAlign: 'center'
+        })}
+      />
     </Stack.Navigator>
   )
 }
