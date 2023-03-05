1d064cb1a1bec833614963747d761dceec8cb75e
Danika Enad <danika.enad@gmail.com>
2023-03-05 15:14:40 -0800

    updated routes and removed irrelevant files

--- src/components/tabs/AppTabs.js
+++ src/components/tabs/AppTabs.js
@@ -1,8 +1,8 @@
 import AttendanceStack from '../stacks/AttendanceStack'
 import EvaluationStack from '../stacks/EvaluationStack';
 import ReportStack from '../stacks/ReportStack';
-import CurriculumScreen from '../screens/CurriculumScreen';
-import StudentsScreen from '../screens/students/StudentsScreen';
+import CurriculumScreen from '../screens/Curriculum/CurriculumScreen';
+import StudentsScreen from '../screens/StudentsScreen';
 
 import { Ionicons } from '@expo/vector-icons'
 import { Icon, View, Box, Text } from 'native-base'
