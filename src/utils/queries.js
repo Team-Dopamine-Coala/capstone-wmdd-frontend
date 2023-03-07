fc875d5c61cbcf0d816fedc8d3b0d6e0242831bc
KEIKO SHIMIZU <106935804+KeikoShimizu@users.noreply.github.com>
2023-03-07 01:01:43 -0800

    new student level code update

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -1,4 +1,37 @@
-import { BACKEND_BASE_URL } from "./static"
+import { BACKEND_BASE_URL, ANDROID_EMU_BASE_URL, AWS_BACKEND_BASE_URL } from "./static"
+
+// CLASSES
+export const getClassesOfCoach = async (coachid) => {
+  if (coachid) {
+    const res = await fetch(`${AWS_BACKEND_BASE_URL}/api/class/${coachid}`)
+    const data = await res.json()
+
+    return data
+  }
+}
+
+export const getSingleClass = async (coachid, classid, userToken) => {
+  if (classid) {
+    const res = await fetch(`${AWS_BACKEND_BASE_URL}/api/class/${coachid}/${classid}`, {
+      headers: {
+        "Authorization": `Bearer ${userToken}`
+      },
+    })
+    const data = await res.json()
+
+    return data
+  }
+}
+// STUDENTS =============================================
+export const getStudentsByClass = async (classid) => {
+  if (classid) {
+    const res = await fetch(`${AWS_BACKEND_BASE_URL}/api/student/${classid}`)
+    const data = await res.json()
+
+    return data
+  }
+}
+
 
 // EVALUATIONS
 export const getEvaluationsByClass = async (classid) => {
@@ -17,3 +50,13 @@ export const getEvaluationsByStudentId = async (studentid) => {
     return data
   }
 }
+
+// ATTENDANCE
+export const getAllAttendance = async () => {
+
+    const res = await fetch(`${AWS_BACKEND_BASE_URL}/api/attendance`)
+    const data = await res.json()
+
+    return data
+}
+// SKILL =============================================
