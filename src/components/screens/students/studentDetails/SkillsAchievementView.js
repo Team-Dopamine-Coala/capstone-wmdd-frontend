97c5a7834e8ec3b3754acbbfbf55a98ebd125ea6
KEIKO SHIMIZU <106935804+KeikoShimizu@users.noreply.github.com>
2023-03-08 10:19:44 -0800

    new queries updated

--- src/components/screens/students/studentDetails/SkillsAchievementView.js
+++ src/components/screens/students/studentDetails/SkillsAchievementView.js
@@ -1,15 +1,26 @@
-import { Text, VStack, View } from "native-base"
+import { Text, VStack, View, Box } from "native-base"
 import { StyleSheet } from "react-native"
 
 
-const SkillsAchievementView = (trainee) => {
+const SkillsAchievementView = ({student, allLevels}) => {
 // console.log('skill',trainee.student)
+// console.log('新しい',student)
+console.log('新しい',allLevels)
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Skill Achievement</Text>
       <VStack>
-        <Text>Current Level</Text>
+        <Text>Hey</Text>
+        {allLevels.map((item, i) => {
+          <Box key={i}>
+            <Text >{item.title}</Text>
+            <Text>{item.skills.length}skills learned</Text>
+            <Text>でないよ</Text>
+          </Box>
+        })}
       </VStack>
+      <Text>Hello</Text>
     </View>
   )
 }
