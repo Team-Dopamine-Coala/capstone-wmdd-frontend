b92672ffcbdb40ee13df1ba2784597a641302aaf
Elmer Balbin Jr <contact@elmer.dev>
2023-03-09 13:02:58 -0800

    reports update

--- src/components/screens/Report/IndexScreen.js
+++ src/components/screens/Report/IndexScreen.js
@@ -4,6 +4,32 @@ const IndexScreen = () => {
   return (
     <View>
       <Heading>This is index screen of Report</Heading>
+
+      {/* <VStack  mb={5} >  
+          <Box  mb={3} p={5} bg="#ffc0cb" width="100%" height="90%" borderRadius="md" shadow={9} position="absolute" top="5%"></Box>
+          <Box ml={4} p={3} bg="#ffffff" flex={1} height="100%" borderRadius="md" shadow={5}>
+            <HStack space={1} mb={2}>      
+              <VStack >
+                <Heading fontSize={22}>{title}</Heading>
+                <Text>{startTime} -{'>'} {endTime}</Text>
+                <Text>Venue</Text>
+              </VStack>
+              <Icon ml="2" size="70" color="gray.200" as={<Ionicons name="checkmark-circle-outline"/>} />         
+            </HStack>
+          
+            <Button
+              dateSelected = {dateSelected}
+              borderRadius="61"
+              variant="solid"
+              bgColor="#404142"
+              onPress={() => {
+                navigation.navigate('Attendance Student List')
+              }}
+            >
+               <Text fontWeight="700" color="#ffffff">Start</Text>
+            </Button>
+          </Box>
+        </VStack> */}
     </View>
   )
 }
