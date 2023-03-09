716d1ccab1eb562127811542a281c45a14ace65d
Elmer Balbin Jr <contact@elmer.dev>
2023-03-09 13:02:58 -0800

    reports update

--- src/components/screens/Report/IndexScreen.js
+++ src/components/screens/Report/IndexScreen.js
@@ -37,63 +37,35 @@ const IndexScreen = ({ navigation }) => {
   }
 
   return (
-    <>
-      <VStack pt="50px" pb={20} flex={1} bgColor="#F4903F" height="100%">
+    <View>
+      <Heading>This is index screen of Report</Heading>
 
-        <Box bgColor="#ffffff" borderTopLeftRadius={20} borderTopRightRadius={20} height="100%">
-          <CalendarStrip
-          scrollable
-          style={{height: 130, paddingTop: 20, paddingBottom: 10}}
-          calendarHeaderStyle={{
-            fontSize: 18,
-            alignSelf: 'flex-start',
-          }}
-          startingDate={moment().subtract(3, 'days')}
-          selectedDate={moment()}
-          onDateSelected={onDateClick}
-          minDate="2023-01-01"
-          maxDate="2023-12-31"
-          dayContainerStyle={{
-            paddingBottom: 4
-          }}
-          dateNameStyle={{
-            marginBottom: 6,
-            fontSize: 13,
-            textTransform: 'capitalize'
-          }}
-          dateNumberStyle={{
-            paddingBottom: 4,
-            fontSize: 16,
-            fontWeight: '300'
-          }}
-          highlightDateNameStyle={{
-            color: '#F4903F',
-            marginBottom: 6,
-            fontSize: 13,
-            textTransform: 'capitalize'
-          }}
-          highlightDateNumberStyle={{
-            color: '#F4903F',
-            fontSize: 16
-          }}
-          highlightDateNumberContainerStyle={{
-          }}
-        />
-        
-        {isLoading ? <Loading /> : <ClassList classes={classes} navigation={navigation} />}
-      </Box>
-
-      </VStack>
-      
-      <BottomSheet
-        isOpen
-        sliderMinHeight={120}
-      >
-        <View>
-          <Text>Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world</Text>
-        </View>
-      </BottomSheet>
-      </>
+      {/* <VStack  mb={5} >  
+          <Box  mb={3} p={5} bg="#ffc0cb" width="100%" height="90%" borderRadius="md" shadow={9} position="absolute" top="5%"></Box>
+          <Box ml={4} p={3} bg="#ffffff" flex={1} height="100%" borderRadius="md" shadow={5}>
+            <HStack space={1} mb={2}>      
+              <VStack >
+                <Heading fontSize={22}>{title}</Heading>
+                <Text>{startTime} -{'>'} {endTime}</Text>
+                <Text>Venue</Text>
+              </VStack>
+              <Icon ml="2" size="70" color="gray.200" as={<Ionicons name="checkmark-circle-outline"/>} />         
+            </HStack>
+          
+            <Button
+              dateSelected = {dateSelected}
+              borderRadius="61"
+              variant="solid"
+              bgColor="#404142"
+              onPress={() => {
+                navigation.navigate('Attendance Student List')
+              }}
+            >
+               <Text fontWeight="700" color="#ffffff">Start</Text>
+            </Button>
+          </Box>
+        </VStack> */}
+    </View>
   )
 }
 
