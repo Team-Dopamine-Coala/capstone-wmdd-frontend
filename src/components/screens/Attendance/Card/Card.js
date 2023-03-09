4a93371dd22234fe826ad41d2611d75a6f60232f
Iris Chen <jaminitaa0322@gmail.com>
2023-03-09 12:04:38 -0800

    update class card UI

--- src/components/screens/Attendance/Card/Card.js
+++ src/components/screens/Attendance/Card/Card.js
@@ -3,14 +3,11 @@ import { Ionicons } from "@expo/vector-icons"
 import { useEffect, useState } from "react";
 
 const Card = ({ title, startTime, endTime, id, navigation, dateSelected, completed, attendances }) => {
-  const [present, setPresent] =  useState()
-  const [absent, setAbsent] =  useState()
-  // const {  title, startTime, endTime, id, navigation } = props;
+  const [present, setPresent] =  useState("-")
+  const [absent, setAbsent] =  useState("-")
   useEffect(() => {
     // TODO: fetch attendance by id
-    // console.log("card - completed:", completed)
-    // console.log("dateSelected",  dateSelected)
-    console.log("attendances:  ", attendances)
+
     let present = 0
     let absent = 0
     if (attendances) {
@@ -19,50 +16,65 @@ const Card = ({ title, startTime, endTime, id, navigation, dateSelected, complet
           present += 1
         } else {
           absent += 1
-        }
+        }      
       });
-      setPresent(present)
-      setAbsent(absent)
-      console.log("present/absent: ", present, absent)
-      
+    }
+
+    if(present==0){
+      setPresent("-")
+    } else {setPresent(present)}
+    if(absent==0){
+      setAbsent("-")
+    } else {
+    setAbsent(absent)
     }
 
   })
   return (
     <>
-      {/* <Box mb={3} p={5} bgColor="#EEF1F4" borderRadius="lg"> */}
         <VStack  mb={5} >  
           <Box  mb={3} p={5} bg="#ffc0cb" width="100%" height="90%" borderRadius="md" shadow={9} position="absolute" top="5%"></Box>
           <Box ml={4} p={3} bg="#ffffff" flex={1} height="100%" borderRadius="md" shadow={5}>
-            <HStack space={1} mb={2}>      
+            <HStack space={1} mb={2} borderBottomWidth="1" pb={2} justifyContent="space-between">      
               <VStack >
-                <Heading fontSize={22}>{title}</Heading>
-                <Text>{startTime} -{'>'} {endTime}</Text>
-                <Text>Venue</Text>
+                  <Heading fontSize={22}>{title}</Heading>
+                  <Text>{startTime} -{'>'} {endTime}</Text>
+                  <Text>Venue</Text>
               </VStack>
-              <Icon ml="2" size="70" color="gray.200" as={<Ionicons name="checkmark-circle-outline"/>} />         
+                  <Icon ml="2" size="70" color={ completed ? "#ffc0cb" : "gray.200"} as={<Ionicons name="checkmark-circle-outline"/>} />         
             </HStack>
-            <HStack space={1}>
-            <Text>Present {present}</Text>
-            <Text>absent {absent}</Text>
-            <Button
-              dateSelected = {dateSelected}
-              borderRadius="61"
-              variant="solid"
-              bgColor="#404142"
-              id={id}
-              onPress={() => {
-                navigation.navigate('Attendance Student List', {
-                  classId: id
-                })
-              }}
-            >
-               <Text  fontWeight="700" color="#ffffff">{ completed ? "View" : "Start"}</Text>
-            </Button>
-            </HStack>
+            <HStack space={1} justifyContent="space-between">
+               <VStack >
+                <Text>{present}</Text>
+                <Text>Present</Text>
+                </VStack >
+                <VStack >
+                <Text>{absent}</Text>
+                <Text>absent</Text>
+                </VStack >
+                <Button
+                  width={100}
+                  dateSelected = {dateSelected}
+                  borderRadius="61"
+                  variant="outline"
+                  bgColor= { completed ? "#eeeeee":"#404142"}
+                  id={id}
+                  onPress={() => {
+                    if(completed){
+                      navigation.navigate('Completed Attendance', {
+                        classId: id
+                    })}
+                    else{
+                    navigation.navigate('Attendance Student List', {
+                      classId: id
+                    })
+                  }}}
+                >
+                  { completed ? <Text  fontWeight="700" color="#404142">View</Text> : <Text  fontWeight="700" color="#ffffff">Start</Text>}
+                </Button>
+              </HStack>
           </Box>
         </VStack>
-      {/* </Box> */}
     </>
   );
 }
