f8061c3f52f15a96fe7c59ad08092589d68d5303
Iris Chen <jaminitaa0322@gmail.com>
2023-03-09 00:05:53 -0800

    Show present/absent data in attendance index

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -17,3 +17,41 @@ export const getEvaluationsByStudentId = async (studentid) => {
     return data
   }
 }
+
+// ATTENDANCE
+export const getAllAttendance = async () => {
+
+    const res = await fetch(`${AWS_BACKEND_BASE_URL}/api/attendance`)
+    const data = await res.json()
+
+    return data
+}
+// SKILL =============================================
+export const getSkillById = async (skillid) => {
+
+  const res = await fetch(`${AWS_BACKEND_BASE_URL}/api/skill/${skillid}`)
+  const data = await res.json()
+
+  return data
+}
+// PROGRAM =============================================
+export const getProgramById = async (programid) => {
+
+  const res = await fetch(`${AWS_BACKEND_BASE_URL}/api/program/${programid}`)
+  const data = await res.json()
+
+  return data
+}
+// LEVEL ===============================================
+export const getLevelById = async (levelid) => {
+
+// ATTENDANCE
+export const getAllAttendance = async (classid) => {
+
+    const res = await fetch(`${AWS_BACKEND_BASE_URL}/api/attendance/${classid}`)
+    const data = await res.json()
+    return data
+}
+
+  return data
+}
