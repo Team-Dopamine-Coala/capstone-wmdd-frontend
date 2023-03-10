3d2a6b90b5a91debd37ae27868f5af03eb16846e
Elmer <contact@elmer.dev>
2023-03-09 17:14:22 -0800

    reports update

--- src/components/screens/Report/IndexScreen.js
+++ src/components/screens/Report/IndexScreen.js
@@ -1,9 +1,103 @@
-import { Text, View, Heading } from 'native-base'
+import { useState, useEffect, useContext } from 'react'
+import { View, Box, Center, Heading, VStack, Text } from 'native-base'
+import CalendarStrip from 'react-native-calendar-strip';
+import moment from 'moment';
+import BottomSheet from 'react-native-simple-bottom-sheet';
 
-const IndexScreen = () => {
+import { AuthContext } from '../../context/AuthContext';
+import ClassList from './lists/ClassList';
+import Loading from '../../layout/Loading'
+
+import { getClassesOfCoach } from '../../../utils/queries';
+
+const IndexScreen = ({ navigation }) => {
+  const {userToken} = useContext(AuthContext)
+  const [classes, setClasses] = useState([])
+  const [dateSelected, setSelectedDate] = useState(new Date())
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    console.log(dateSelected)
+
+    setIsLoading(true)
+    getClassesOfCoach('63fcf0bd354e8150f45dd4d2', userToken).then(
+      data => {
+        setClasses(data)
+        setIsLoading(false)
+      },
+      error => {
+        throw error
+      }
+    )
+  }, [dateSelected])
+  
+
+  const onDateClick = (date) => {
+    setSelectedDate(date)
+  }
+
   return (
-    <View>
-      <Heading>This is index screen of Report</Heading>
+    <>
+      <VStack pt="50px" pb={20} flex={1} bgColor="#F4903F" height="100%">
+
+        <Box bgColor="#ffffff" borderTopLeftRadius={20} borderTopRightRadius={20} height="100%">
+          <CalendarStrip
+          scrollable
+          style={{height: 130, paddingTop: 20, paddingBottom: 10}}
+          calendarHeaderStyle={{
+            fontSize: 18,
+            alignSelf: 'flex-start',
+          }}
+          startingDate={moment().subtract(3, 'days')}
+          selectedDate={moment()}
+          onDateSelected={onDateClick}
+          minDate="2023-01-01"
+          maxDate="2023-12-31"
+          dayContainerStyle={{
+            paddingBottom: 4
+          }}
+          dateNameStyle={{
+            marginBottom: 6,
+            fontSize: 13,
+            textTransform: 'capitalize'
+          }}
+          dateNumberStyle={{
+            paddingBottom: 4,
+            fontSize: 16,
+            fontWeight: '300'
+          }}
+          highlightDateNameStyle={{
+            color: '#F4903F',
+            marginBottom: 6,
+            fontSize: 13,
+            textTransform: 'capitalize'
+          }}
+          highlightDateNumberStyle={{
+            color: '#F4903F',
+            fontSize: 16
+          }}
+          highlightDateNumberContainerStyle={{
+          }}
+        />
+        
+        {isLoading ? <Loading /> : <ClassList classes={classes} navigation={navigation} />}
+      </Box>
+
+      </VStack>
+      
+      <BottomSheet
+        isOpen
+        sliderMinHeight={120}
+      >
+        <View>
+          <Text>Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world</Text>
+        </View>
+      </BottomSheet>
+      </>
+  )
+}
+
+export default IndexScreen
 
       {/* <VStack  mb={5} >  
           <Box  mb={3} p={5} bg="#ffc0cb" width="100%" height="90%" borderRadius="md" shadow={9} position="absolute" top="5%"></Box>
@@ -30,8 +124,3 @@ const IndexScreen = () => {
             </Button>
           </Box>
         </VStack> */}
-    </View>
-  )
-}
-
-export default IndexScreen
---
+++ src/components/screens/Report/lists/ClassList.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react"
+import { Box, FlatList, Text } from "native-base"
+import ClassListItem from "../listItem/ClassListItem"
+
+const ClassList = ({ classes, navigation }) => {
+  return (
+    <FlatList
+      data={classes}
+      renderItem={({ item }) => (
+        <ClassListItem item={item} navigation={navigation} />
+      )}
+      keyExtractor={item => item._id}
+      showsHorizontalScrollIndicator={false}
+    />
+  )
+}
+
+export default ClassList
