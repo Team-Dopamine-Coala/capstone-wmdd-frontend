a8635a9698f69b43ab5314f12fe54741c5738628
KEIKO SHIMIZU <106935804+KeikoShimizu@users.noreply.github.com>
2023-03-09 21:40:03 -0800

    skill fetch queries are update

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -60,3 +60,27 @@ export const getAllAttendance = async (classid) => {
     return data
 }
 // SKILL =============================================
+export const getSkillById = async (skillid) => {
+  
+  const res = await fetch(`${AWS_BACKEND_BASE_URL}/api/skill/${skillid}`)
+  const data = await res.json()
+
+  return data
+}
+
+// PROGRAM =============================================
+export const getProgramById = async (programid) => {
+
+  const res = await fetch(`${AWS_BACKEND_BASE_URL}/api/program/${programid}`)
+  const data = await res.json()
+
+  return data
+}
+// LEVEL ===============================================
+export const getLevelById = async (levelid) => {
+
+  const res = await fetch(`${AWS_BACKEND_BASE_URL}/api/level/${levelid}`)
+  const data = await res.json()
+
+  return data
+}
