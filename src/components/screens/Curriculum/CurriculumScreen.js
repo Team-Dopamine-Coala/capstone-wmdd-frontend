18a20b5b97b1285414930b3da9cf7bf41f00c822
Danika Enad <danika.enad@gmail.com>
2023-03-09 21:16:23 -0800

    updated bottom sheet

--- src/components/screens/Curriculum/CurriculumScreen.js
+++ src/components/screens/Curriculum/CurriculumScreen.js
@@ -1,16 +1,36 @@
 import { View, Button, VStack, Box } from "native-base"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { fetchSkills } from '../../../utils/queries';
 
 import Loading from '../../layout/Loading'
 import SkillsList from '../Curriculum/lists/SkillsList'
 
 const CurriculumScreen = ({ navigation }) => {
+  const [skills, setSkills] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+      setIsLoading(true)
+      fetchSkills().then(
+          data => {
+              setSkills(data)
+              setIsLoading(false)
+          }, 
+          error => {
+              throw error
+          }
+      )
+  }, [])
+
+  useEffect(() => {
+      console.log(skills)
+  }, [skills])
 
   return (
     <VStack pt="50px" pb={20} flex={1} bgColor="#F4903F">
       <Box bgColor="#ffffff" borderTopLeftRadius={20} borderTopRightRadius={20}>
-        {/* { isLoading ? <Loading /> : <SkillsList />} */}
-        <SkillsList />
+        { isLoading ? <Loading /> : <SkillsList skills={skills} />}
+        {/* <SkillsList /> */}
       </Box>
     </VStack>
   )
