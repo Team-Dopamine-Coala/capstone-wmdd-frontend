ff25170888b8cb454b6e1e4f928badedd029bb5c
KEIKO SHIMIZU <106935804+KeikoShimizu@users.noreply.github.com>
2023-03-28 23:18:58 -0700

    calender code completed

--- src/components/screens/Setting/Class/WeekCalender.js
+++ src/components/screens/Setting/Class/WeekCalender.js
@@ -1,43 +1,44 @@
 import { View, Text, HStack, Box } from 'native-base'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { StyleSheet } from "react-native"
 
 const WeekCalender = ({weektitle}) => {
     console.log('this WEEK',weektitle)
-
+    
     const calender = [
-        {week: "Monday", title: "M"},
-        {week: "Tuesday", title: "T"},
-        {week: "Wednesday", title: "W"},
-        {week: "Thursday", title: "T"},
-        {week: "Friday", title: "F"},
-        {week: "Saturday", title: "S"},
-        {week: "Sunday", title: "S"}
+            {week: "Monday", title: "M"},
+            {week: "Tuesday", title: "T"},
+            {week: "Wednesday", title: "W"},
+            {week: "Thursday", title: "T"},
+            {week: "Friday", title: "F"},
+            {week: "Saturday", title: "S"},
+            {week: "Sunday", title: "S"}
     ]
-    console.log(calender)
+    
     return (
         <>
             <HStack style={styles.container}>
-                {calender.map((item,i) => {
+                {calender.map((item,i) => (
                     <Box key={i} style={styles.circle} bg={weektitle == item.week ? "#FFFFFF" : "#BBA0EC"} > 
                         <Text fontFamily="Lexend_400" 
                                 fontSize={14} 
-                                lineHeight={21} 
+                                lineHeight={30} 
                                 alignSelf={'center'} 
-                                justifyItems={'center'} 
+                                justifyContent={'center'}
                                 color="#000000"
                         >
                             {item.title}
                         </Text>    
                     </Box>
-                })}
+                ))}
             </HStack>
         </> 
     )
 }
 const styles = StyleSheet.create ({
     container: {
-        alignSelf:'center',
+        justifyContent:'space-around',
+        paddingHorizontal: 24,
     },
     circle:{  
         width:30,
