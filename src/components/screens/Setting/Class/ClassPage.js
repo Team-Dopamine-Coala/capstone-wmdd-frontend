781c70de5e774637890debe9743b46d456aedde8
KEIKO SHIMIZU <106935804+KeikoShimizu@users.noreply.github.com>
2023-03-28 18:27:49 -0700

    css added

--- src/components/screens/Setting/Class/ClassPage.js
+++ src/components/screens/Setting/Class/ClassPage.js
@@ -134,10 +134,8 @@ const ClassPage = ({navigation, route}) => {
 }
 const styles = StyleSheet.create ({
   container: {
-    container: {
       paddingVertical: 10,
       marginHorizontal: 5,
-    },
   },
   background: {
     backgroundColor: '#FDFDFD',
@@ -168,70 +166,5 @@ const styles = StyleSheet.create ({
   title: {
    textAlign: 'center',
   }
-  
 })
 export default ClassPage
-
-{/* <View style={styles.container}>
-      <VStack>
-        <Box mb={3} p={5} bg="#F5D26A" width="100%" height="90%" borderRadius="md" shadow={9} position="absolute" top="5%"></Box>  
-          <Box ml={4} p={3} bg="#ffffff" flex={1} height="100%" borderRadius="md" shadow={5}>
-        
-          <Heading style={styles.title}>{classTitle}</Heading>
-          <Box style={styles.levelbox}>
-            <Text style={styles.levelname}>{classCard.level}</Text>
-            <Box style={styles.percentBox}>
-              <Box>
-                
-              </Box>
-              <Box style={styles.numberBox}>
-                <Text style={styles.current}>{completedSkillNbr}</Text>
-                <Text style={styles.total}> / {totalSkillNbr} skills</Text>
-              </Box>
-            </Box>
-          </Box>
-        
-        </Box>
-      </VStack>
-    </View>
-      )
-}
-const styles = StyleSheet.create ({
-  container: {
-    paddingVertical: 10,
-    // backgroundColor: '#ffffff',
-    marginHorizontal: 5,
-  },
-  whitebox: {
-    height: 60,
-    flex:1,
-    marginLeft: 20,
-    backgroundColor: '#ffffff',
-    height: '100%',
-    borderRadius: 10,
-    shadow: 5,
-  },
-  contents: {
-    marginTop: 10,
-    marginHorizontal: 20,
-    marginBottom: 20,
-  },
-  title: {
-   textAlign: 'center',
-  },
-  levelname: {
-    fontSize: 16,
-    fontWeight: '500',
-  },
-  percentBox: {
-    flexDirection: "row",
-    justifyContent:"space-around",
-  },
-  numberBox: {
-    flexDirection: "row",
-    marginLeft: 16,
-  },
-  current: {
-    fontSize: 23,
-    fontWeight: "bold",
-  } */}
