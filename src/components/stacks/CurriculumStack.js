771e5146a66154897a4f294434df9402ad711f44
Danika Enad <danika.enad@gmail.com>
2023-03-29 16:31:19 -0700

    added profile image to header

--- src/components/stacks/CurriculumStack.js
+++ src/components/stacks/CurriculumStack.js
@@ -1,6 +1,7 @@
 import { createStackNavigator, TransitionPresets } from "@react-navigation/stack"
 import CurriculumScreen from '../screens/Curriculum/CurriculumScreen'
 import SkillList from "../screens/Curriculum/lists/SkillList";
+import HeaderImage from "../layout/HeaderImage";
   
 const Stack = createStackNavigator()
 
@@ -11,8 +12,8 @@ const TransitionScreenOptions = {
 const CurriculumStack = ({route}) => {
     return (
       <Stack.Navigator screenOptions={TransitionScreenOptions}>
-        <Stack.Screen name="Curriculum Index" component={CurriculumScreen}       
-        options={{
+        <Stack.Screen name="Curriculum Index" component={CurriculumScreen}    
+        options={({ navigation }) => ({
           title: 'Curriculum',
           headerTitleAlign: 'center',
           headerTransparent: true,
@@ -21,9 +22,12 @@ const CurriculumStack = ({route}) => {
             fontFamily: 'Lexend_700',
             fontSize: 20
           },
-        }} />
+          headerRight: () => (
+            <HeaderImage navigations={navigation}/>
+          )
+        })} />
         <Stack.Screen name="Skill List" component={SkillList}
-          options={({ route }) => ({
+          options={({ route, navigation }) => ({
             title: `${route.params.name}`,
             skills: route.params.skills,
             headerTitleAlign: 'center',
@@ -33,6 +37,10 @@ const CurriculumStack = ({route}) => {
               fontFamily: 'Lexend_700',
               fontSize: 20
             },
+            headerRight: () => (
+              <HeaderImage navigations={navigation}/>
+            ),
+            headerBackTitleVisible: false
           })} />
       </Stack.Navigator>
       
