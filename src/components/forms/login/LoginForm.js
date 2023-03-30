3ba0eaeca3890c92ad9a1298fc0d95689ef40eca
Danika Enad <danika.enad@gmail.com>
2023-03-30 13:06:57 -0700

    updated login styling

--- src/components/forms/login/LoginForm.js
+++ src/components/forms/login/LoginForm.js
@@ -1,4 +1,4 @@
-import { FormControl, Input, Text, VStack, Icon, Pressable, Button, Box, Link } from 'native-base';
+import { FormControl, Input, Text, VStack, Icon, Pressable, Box, Button, Link } from 'native-base';
 import { MaterialIcons } from "@expo/vector-icons";
 import React, { useState, useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
@@ -12,32 +12,57 @@ const LoginForm = ({navigation}) => {
     const {login} = useContext(AuthContext)
 
     return(
-        <VStack justifyContent="center" space={2} py={30} px={15}>
-            <Text fontSize="3xl" mt={100}>Welcome Back!</Text>
-            <Text fontSize="xl">Log in to continue</Text>
+        <VStack justifyContent="center" pl={8} pr={8}>
+            <Text fontFamily="Lexend_700" fontSize={28} mt={20}>Welcome Back!</Text>
+            <Text fontFamily="Lexend_500" fontSize={12}>Log In to continue</Text>
             <FormControl isRequired>
-                <Box py={2}>
+                <Box py={2} mt={20}>
                 <Input 
                     placeholder="Email Address"
                     variant="underlined"
                     onChangeText={(e) => setEmail(e)}
                     value={email}
+                    style={{
+                        fontFamily:"Lexend_400",
+                        color:"#667080"
+                    }}
                 />
                 </Box>
-                <Box py={2}>
+                <Box py={2} mt={4}>
                 <Input 
                     placeholder="Password"
                     variant="underlined"
                     type={showPassword ? "text" : "password"} 
-                    InputRightElement={<Pressable onPress={() => setShowPassword(!showPassword)}><Icon as={<MaterialIcons name={showPassword ? "visibility" : "visibility-off"} />} size={5} mr="2" color="muted.400" /></Pressable>} 
+                    InputRightElement={<Pressable onPress={() => setShowPassword(!showPassword)}><Icon as={<MaterialIcons name={showPassword ? "visibility" : "visibility-off"} />} size={5} mr="2" color="#667080" /></Pressable>} 
                     onChangeText={(e) => setPassword(e)}
                     value={password}
+                    style={{
+                        fontFamily:"Lexend_400",
+                        color:"#737373"
+                    }}
                 />
                 </Box>
-                <Button mt={5} onPress={() => {login(email, password)}}>Login</Button>
-                <Link mt={5}>Forgot your password?</Link>
+                <Button onPress={() => {login(email, password)}}
+                    bg="#FE7F2D"
+                    mt={5}
+                    _text={{
+                    fontFamily:"Lexend_700",
+                    }}
+                >Login</Button>
+                <Link 
+                    mt={5} 
+                    justifyContent='flex-end'
+                    _text={{
+                        textAlign:"left",
+                        fontFamily:"Lexend_500",
+                        color:"#667080",
+                    }}
+                >Forgot your password?</Link>
                 <Text bold fontSize="sm" mt={7}>OR</Text>
-                <Button mt={7}>Google</Button>
+                <Button 
+                    mt={7}
+                    bg="#FE7F2D"
+                >Google</Button>
                 <Text mt={5}>New to Coala? <Link onPress={() => navigation.navigate('Sign up one') }> Sign up now</Link></Text>
             </FormControl>
         </VStack>
