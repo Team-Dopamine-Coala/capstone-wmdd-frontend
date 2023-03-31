97a2b8b032a04a6240c5d05d74224f21a9571555
Danika Enad <danika.enad@gmail.com>
2023-03-31 09:56:41 -0700

    updated styling

--- src/components/stacks/LoginSignUpStack.js
+++ src/components/stacks/LoginSignUpStack.js
@@ -2,30 +2,42 @@ import LoginScreen from '../screens/Login/LoginScreen'
 import SignUpScreenOne from '../screens/SignUp/SignUpScreenOne';
 import SignUpScreenTwo from '../screens/SignUp/SignUpScreenTwo';
 
-import { createStackNavigator } from "@react-navigation/stack"
+import { createStackNavigator, TransitionPresets } from "@react-navigation/stack"
 
 
 const Stack = createStackNavigator()
 
+const TransitionScreenOptions = {
+  ...TransitionPresets.SlideFromRightIOS
+}
+
 const LoginSignUpStack = () => {
 
   return (
-        <Stack.Navigator
-            screenOptions={{
-            headerShown: false,
-            }}
-        >
+        <Stack.Navigator screenOptions={TransitionScreenOptions}>
             <Stack.Screen
             name="Login"
             component={LoginScreen}
+            options={{
+              headerShown: false
+            }}
             />
             <Stack.Screen
             name="Sign up one"
             component={SignUpScreenOne}
+            options={{
+              headerShown: false
+            }}
             />
             <Stack.Screen 
-            name="Sign up two"
+            name=" "
             component={SignUpScreenTwo}
+            options={{
+              headerBackTitleVisible: false,
+              headerTintColor: "#737373",
+              headerTitleVisible: false,
+              headerTransparent: true,
+            }}
             />
         </Stack.Navigator>
   )
