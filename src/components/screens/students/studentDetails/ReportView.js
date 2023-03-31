f84c355de4b23aaead52aeb33aae79429bc3c4ba
KEIKO SHIMIZU <106935804+KeikoShimizu@users.noreply.github.com>
2023-03-30 23:35:58 -0700

    biometric click tag css and icon added

--- src/components/screens/students/studentDetails/ReportView.js
+++ src/components/screens/students/studentDetails/ReportView.js
@@ -1,4 +1,4 @@
-import { View, Text, Icon, VStack } from "native-base"
+import { View, Text, Box, Icon, VStack } from "native-base"
 import { useState } from "react"
 import { StyleSheet, Modal, TouchableOpacity } from "react-native"
 import { Ionicons } from '@expo/vector-icons'
@@ -14,9 +14,12 @@ const clickStudent = () => {
 const closeBio = () => {setModalIsOpen(false)}
 return (
     <View >
-        <TouchableOpacity onPress={clickStudent} trainee={student} style={styles.container}>
-            <Text style={styles.name} fontFamily="Lexend_700">{student.firstname} {student.lastname}</Text> 
-            <Text style={styles.text} fontFamily="Lexend_400">View profile and Contact Information</Text>
+        <TouchableOpacity onPress={clickStudent} trainee={student} style={styles.container} shadow={5}>
+            <VStack>
+                <Text style={styles.name} fontFamily="Lexend_700">{student.firstname} {student.lastname}</Text> 
+                <Text style={styles.text} fontFamily="Lexend_400">View profile and Contact Information</Text>
+            </VStack>
+            <Icon size={4} as={<Ionicons name='chevron-forward-outline' />} style={styles.icon}/>
         </TouchableOpacity>
         <Modal visible={modalIsOpen}>
             <View style={styles.biobackground}>
@@ -29,25 +32,35 @@ return (
 const styles = StyleSheet.create ({
     container: {
         backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        borderWidth: 1,
         borderColor: 'rgba(238, 238, 238, 0.5)',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,    
         paddingHorizontal: 16,
         paddingVertical: 10,
         borderRadius: 12,
         marginHorizontal: 20,
         marginTop: 16,
         marginBottom: 22,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
     },
     name: {
         color: '#212427',
         fontSize: 28,
-        // fontWeight: "700",
         lineHeight: 30,
     },
     text: {
         marginTop: 6,
         color: '#212427',
         fontSize: 16,
-        // fontWeight: "400",
         lineHeight: 20,
     },
     icon:{
@@ -56,6 +69,7 @@ const styles = StyleSheet.create ({
         lineHeight: 18,
         width: 15,
         height: 18,
+        
     },
     biobackground:{
         flex:1,
