2c5c386340e3af82a9e471c369786d6d7fc510c8
KEIKO SHIMIZU <106935804+KeikoShimizu@users.noreply.github.com>
2023-03-30 23:35:58 -0700

    biometric click tag css and icon added

--- src/components/screens/students/studentDetails/ReportView.js
+++ src/components/screens/students/studentDetails/ReportView.js
@@ -1,6 +1,7 @@
-import { View, Text, Box } from "native-base"
+import { View, Text, Box, Icon, VStack } from "native-base"
 import { useState } from "react"
 import { StyleSheet, Modal, TouchableOpacity } from "react-native"
+import { Ionicons } from '@expo/vector-icons'
 import StudentBiometrics from "../Biometrics/StudentBiometrics"
 
 const ReportView = ({student, navigation}) => {
@@ -15,9 +16,12 @@ const clickStudent = () => {
 const closeBio = () => {setModalIsOpen(false)}
 return (
     <View >
-        <TouchableOpacity onPress={clickStudent} trainee={student} style={styles.container}>
-            <Text style={styles.name} fontFamily="Lexend_700">{student.firstname} {student.lastname}</Text> 
-            <Text style={styles.text} fontFamily="Lexend_400">View profile and Contact Information</Text>
+        <TouchableOpacity onPress={clickStudent} trainee={student} style={styles.container} shadow={5}>
+            <VStack>
+                <Text style={styles.name} fontFamily="Lexend_700">{student.firstname} {student.lastname}</Text> 
+                <Text style={styles.text} fontFamily="Lexend_400">View profile and Contact Information</Text>
+            </VStack>
+            <Icon size={4} as={<Ionicons name='chevron-forward-outline' />} style={styles.icon}/>
         </TouchableOpacity>
         <Modal visible={modalIsOpen}>
             <Box style={styles.biobackground}>
@@ -30,26 +34,44 @@ return (
 const styles = StyleSheet.create ({
     container: {
         backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        borderWidth: 1,
         borderColor: 'rgba(238, 238, 238, 0.5)',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,    
         paddingHorizontal: 16,
         paddingVertical: 10,
         borderRadius: 12,
         marginHorizontal: 20,
         marginTop: 16,
         marginBottom: 22,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
     },
     name: {
         color: '#212427',
         fontSize: 28,
-        // fontWeight: "700",
         lineHeight: 30,
     },
     text: {
         marginTop: 6,
         color: '#212427',
         fontSize: 16,
-        // fontWeight: "400",
         lineHeight: 20,
+    },
+    icon:{
+        color: '#212427',
+        fontSize: 20,
+        lineHeight: 18,
+        width: 15,
+        height: 18,
+        
     },
     biobackground:{
         backgroundColor:'#bbb',
