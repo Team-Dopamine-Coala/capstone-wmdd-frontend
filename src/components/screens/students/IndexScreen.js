67cf61dea6f6aa75c1227f26a8eddbb525731cb6
KEIKO SHIMIZU <106935804+KeikoShimizu@users.noreply.github.com>
2023-04-15 10:53:33 -0700

    header margin adjusted, progressbar fixed

--- src/components/screens/students/IndexScreen.js
+++ src/components/screens/students/IndexScreen.js
@@ -115,8 +115,8 @@ const styles = StyleSheet.create ({
     paddingHorizontal: 20,
     borderTopRightRadius:28,
     borderTopLeftRadius:28,
-    marginTop: 98,
     height: '100%',
+    marginTop: 99,
   },
   scrollarea: {
     marginTop: 24,
