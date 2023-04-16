b1c022df4af0d60f19fa478e317699f4620ecdad
Elmer Balbin Jr <contact@elmer.dev>
2023-04-15 21:11:03 -0700

    progress circles fix

--- src/components/screens/Evaluation/listItem/ClassListItem.js
+++ src/components/screens/Evaluation/listItem/ClassListItem.js
@@ -1,9 +1,30 @@
+import { useState, useEffect } from 'react'
 import { Ionicons } from '@expo/vector-icons'
 import { Box, HStack, VStack, Text, Button, Heading, View, Icon } from 'native-base'
 import moment from 'moment'
 import ProgressCircle from 'react-native-progress-circle'
+import { getStudentsByClass } from '../../../../utils/queries'
 
 const ClassListItem = ({ item, navigation, calendarDate }) => {
+  const [total, setTotal] = useState(0)
+  const [pending, setPending] = useState(0)
+  const [completed, setCompleted] = useState(0)
+
+  useEffect(() => {
+    getStudentsByClass(item._id).then(
+      data => {
+        setTotal(data?.length)
+        setPending(data?.filter(item => item.evaluated == 0).length)
+        setCompleted(data?.filter(item => item.evaluated == 1).length)
+      },
+      error => {
+        throw error
+      }
+    ).catch((error) => {
+      throw error
+    })
+  }, [item])
+
   return (
     <View mx={4} my={4}>
       <Box  mb={3} p={5} bg={item.color} width="100%" height="90%" borderRadius="md" shadow={9} position="absolute" top="5%"></Box>
@@ -12,14 +33,14 @@ const ClassListItem = ({ item, navigation, calendarDate }) => {
           <HStack borderBottomColor="#737373" borderBottomWidth={1} mb={3}>
             <Box pr={3} pb={3}>
               <ProgressCircle
-                  percent={80}
+                  percent={completed / total * 100}
                   radius={50}
                   borderWidth={10}
                   color={item.color}
                   shadowColor="#D0CFD4"
                   bgColor={item.cardColor}
               >
-                <Text style={{ fontSize: 20, fontFamily: 'Lexend_700' }}>3/8</Text>
+                <Text style={{ fontSize: 20, fontFamily: 'Lexend_700' }}>{completed}/{total}</Text>
               </ProgressCircle>
             </Box>
             <VStack space={1} mb={2} ml={1} pt={1}>
@@ -39,11 +60,11 @@ const ClassListItem = ({ item, navigation, calendarDate }) => {
 
           <HStack>
             <VStack flex={1} alignItems="center">
-              <Text fontFamily="Lexend_700" fontSize={20} color="#F35242">5</Text>
+              <Text fontFamily="Lexend_700" fontSize={20} color="#F35242">{pending}</Text>
               <Text fontFamily="Lexend_400" fontSize={14} color="#737373">Pending</Text>
             </VStack>
             <VStack flex={1} alignItems="center">
-              <Text fontFamily="Lexend_700" fontSize={20}>3</Text>
+              <Text fontFamily="Lexend_700" fontSize={20}>{completed}</Text>
               <Text fontFamily="Lexend_400" fontSize={14} color="#737373">Completed</Text>
             </VStack>
             <Button
@@ -58,7 +79,7 @@ const ClassListItem = ({ item, navigation, calendarDate }) => {
                   calendarDate: calendarDate
                 })
               }}
-            ><Text fontFamily="Lexend_700" fontSize={16} color="#ffffff">Start</Text></Button>
+            ><Text fontFamily="Lexend_700" fontSize={16} color="#ffffff">{pending > 0 ? 'Start' : 'View'}</Text></Button>
           </HStack>
 
         </VStack>
